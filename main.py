210995a1e3f957434d9ef22e6d4562fa157f4ec6
Nestor Alban <albquenes7294gmail.com>
2022-02-12 05:13:33 -0500

    users_singup_docstrings

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ class User(UserBase):
     )
     birth_date: Optional[date]= Field(default=None)
 
+class UserRegister(User):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
+
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
     content: str = Field(
@@ -66,7 +73,20 @@ class Tweet(BaseModel):
     tags=["Users"]
     )
 def singup():
-    pass
+    """
+    This path operation register a user in the app
+
+    Parameters: 
+        - Request body parameter
+            - user: UserRegister
+    
+    Returns a json with the basic user information: 
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: str
+    """
 
 ###Login a User
 
